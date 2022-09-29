7ce773992571cb749efb2549e9b7c2a1221789dd
Omar Faruk <farukk6319@gmail.com>
2022-09-29 21:47:08 +0600

    footer added

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.css'
+const Footer = () => {
+  return (
+    <div className='footer'>
+       <p>Copyright © 2022 FPW's</p>
+    </div>
+  );
+};
+
+export default Footer;
